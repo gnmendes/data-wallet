4f1dd06b3e33df327130ba5c647a9ffeb35613a9
Gabriel Mendes <nscbiel@gmail.com>
2020-09-28 20:08:24 -0300

    Verificando se o campo foi enviado e se ele é válido

--- scripts/transactional/transactional_controller.py
+++ scripts/transactional/transactional_controller.py
@@ -1,8 +1,10 @@
-from flask import Flask
-
+from flask import Flask, request
+from scripts.common.utilities import CPFValidator as cpf, InputValidator
 app = Flask(__name__)
 
 
 @app.route('introduce_data', method=['POST'])
 def create_something():
+    assert InputValidator.validate_input(required_parameters=['cpf'], incoming_data=request.get_json())
+    cpf.is_cpf_valid(cpf=request.get_json()['cpf'])
     return None
