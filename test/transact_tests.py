c11be0f29cd2ee65e00d23509243b863130a99ab
Gabriel Mendes <nscbiel@gmail.com>
2020-10-26 23:03:31 -0300

    Adição de testes unitários para dados transacionaveis

--- test/transact_tests.py
+++ test/transact_tests.py
@@ -19,6 +19,20 @@ class TransactionalTestCase(unittest.TestCase):
         self.assertEqual(201, response.status_code, 'O status code deve ser 201 indicando que a transação foi adicionada')
         self.assertIn('transação não confirmada adicionada', response.json['message'].lower())
 
+    def test_deve_minerar_um_bloco(self):
+        response = self.app_client.get('/chain/mine')
+
+        self.assertEqual(200, response.status_code, 'O bloco deve ter sido minerado')
+        self.assertEqual(2, response.json['index'], 'O indice deve ser o 2')
+        self.assertEqual('New block forged!', response.json['message'], 'A mensagem quando minerado com sucesso'
+                                                                       'deve corresponder')
+
+    def test_deve_trazer_a_representacao_cadeia(self):
+        response = self.app_client.get('/chain/representation')
+
+        self.assertEqual(200, response.status_code, 'O status deve indicar que a requisicao ocorreu OK')
+        self.assertEqual(1, response.json['length'], 'O comprimento da cadeia deve corresponder')
+
     @staticmethod
     def __get_unconfirmed_transact():
         return {
